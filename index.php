b823fa2bc20ecd69d1702d378fec52f31024b886
unknown <matheus.sousa247@hotmail.com>
2019-11-17 17:03:08 -0300

    Coding a footer, need to finish yet

--- index.php
+++ index.php
@@ -5,22 +5,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/style.css" />
-        <title>Modal</title>
+        <link rel="stylesheet" href="css/style.css">
+        <title>BeyMind</title>
     </head>
     <body>
         <header>
-            <div class="container" id="navbar">
-                <h1 class="teste">Teste header</h1>
-            </div>
+            <p>HEADER</p>
         </header>
         <main>
-
+            <p>MAIN</p>
         </main>
         <aside>
-            
+            <p>ASIDE</p>
         </aside>
-        <?php include_once("inc/footer.html"); ?>
+        <footer>FOOTER</footer>
 
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
