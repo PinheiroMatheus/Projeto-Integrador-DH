9109ae6453b5c1247e6bc8073e9f7ee3724a0ad7
unknown <matheus.sousa247@hotmail.com>
2019-11-18 19:18:35 -0300

    Updating the code

---
+++ inc/footer.php
@@ -0,0 +1,37 @@
+<footer class="container-fluid fixed-bottom">
+    <section class="row p-3">
+        <article class="col-3">
+            <h3 class="text-center my-auto">BeyMind</h3>
+        </article>
+        <article class="col-3">
+            <h5>Sobre a gente</h5>
+            <ul>
+                <li><a href="">Quem somos</a></li>
+                <li><a href="">História</a></li>
+                <li><a href="">Sobre nós</a></li>
+                <li><a href="">Da gente</a></li>
+            </ul>
+        </article>
+        <article class="col-3">
+            <h5>Sobre a gente</h5>
+            <ul>
+                <li><a href="">Quem somos</a></li>
+                <li><a href="">História</a></li>
+                <li><a href="">Sobre nós</a></li>
+                <li><a href="">Da gente</a></li>
+            </ul>
+        </article>
+        <article class="col-3">
+            <h5>Sobre a gente</h5>
+            <ul>
+                <li><a href="">Quem somos</a></li>
+                <li><a href="">História</a></li>
+                <li><a href="">Sobre nós</a></li>
+                <li><a href="">Da gente</a></li>
+            </ul>
+        </article>
+    </section>        
+    <section class="footer2 row py-3">
+        <p id="texto-footer2" class="col-12 text-center m-0">Copywriting 2019 - Todos os direitos reservados</p>
+    </section>
+</footer>
