679aafa2d0dc63efb4947e13255c1fa3f69159b2
unknown <matheus.sousa247@hotmail.com>
2019-11-25 19:13:10 -0300

    fix Checkout

--- login.php
+++ login.php
@@ -10,20 +10,23 @@
     <title>Login</title>
 </head>
 <body id="login">
-        <div class="container text-center pt-5 pb-5 mt-5 px-4" id="cont-login1">
-            <h2>Logo</h2>
-        </div class="container" id="test">
-        <div class="container" id="cont-login2">
+        <!-- <div class="container text-center pt-5 pb-5 mt-5 px-4" id="cont-login1">
+            <img src="" alt="image.php" id="image">
+        </div class="container" id="test"> -->
+        <div class="media container pt-5 center-text pb-5 mt-5 px-4">
+            <img src="..." alt="..." class="img-thumbnail rounded mx-auto d-block" id="img1">
+        </div>
+        <div class="container px-4" id="cont-login2">
             <form>
-                <div class="form-group pt-5 px-4">
+                <div class="form-group pt-5 ">
                   <label for="exampleInputEmail1"></label>
                   <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite seu email">
                 </div>
-                <div class="form-group px-4">
+                <div class="form-group ">
                   <label for="exampleInputPassword1"></label>
                   <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Digite sua senha">
                 </div>
-                <div class="pt-2 pb-2 px-4">
+                <div class="pt-2 pb-2 ">
                     <a href="#">Esqueci minha senha</a>
                 </div>
                 <button type="submit" class="btn btn-primary px-4 btn-block mt-3">Entrar</button>
