1b59e1dd0953675c973e30fafa6ccd1542bfea4b
unknown <matheus.sousa247@hotmail.com>
2019-11-25 19:37:56 -0300

    fix Footer.php/Login.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,10 +1,10 @@
 <footer>
     <div class="container-fluid fixed-bottom" id="row1">
         <div class="row">
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 pt-2">
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 pt-5">
                     <h3 class="text-center my-auto">BeyMind</h3>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-4 pt-2">
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-4 pb-5 pt-5">
                     <h5 id="h5-footer"><b>Sobre a gente</b></h5>
                     <ul id="ul-footer">
                         <li><a href="#" id="a-footer">Quem somos</a></li>
@@ -13,7 +13,7 @@
                         <li><a href="#" id="a-footer">Da gente</a></li>
                     </ul>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-4 pt-2">
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-4 pb-5 pt-5">
                     <h5 id="h5-footer"><b>Sobre a gente</b></h5>
                     <ul id="ul-footer">
                         <li><a href="#" id="a-footer">Quem somos</a></li>
@@ -22,7 +22,7 @@
                         <li><a href="#" id="a-footer">Da gente</a></li>
                     </ul>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-4 pt-2">
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-4 pb-5 pt-5">
                     <h5 id="h5-footer"><b>Sobre a gente</b></h5>
                     <ul id="ul-footer">
                         <li><a href="#" id="a-footer">Quem somos</a></li>
