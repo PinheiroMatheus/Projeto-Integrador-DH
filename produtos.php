5dcbb24c69ca8a21214f7efb0724d4b91dba614e
kaiquemaia <kaiquefeng1@gmail.com>
2019-12-02 01:25:41 -0300

    Created page products

--- produtos.php
+++ produtos.php
@@ -6,10 +6,45 @@
         <?php $title = "Produtos"; ?>
 
 <body>
+
+<!-- Require header -->
 <?php require_once("inc/header.php") ?>
 
+<div class="row product">
 
 
+<div class="col-sm container pt-5 ">
+
+<img src="assets/img/product.png" class=" rounded mx-auto d-block img-fluid "alt="" width="60%">
+
+</div>
+
+<div class="col-sm container pt-5 px-5 align-middle">
+        <h1>Lorem Ipsum</h1>
+        <p class="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero atque, quam eveniet temporibus dignissimos dicta velit facere magni? Consequatur neque, earum magnam saepe sit fuga incidunt inventore repellendus ex suscipit?</p>
+        <button type="button" class="btn btn-light">CONTRATAR AGORA</button>
+</div>
+
+</div>
+
+<div class="container pt-4 pl-4">
+        <h2>Lorem ipsum</h2>
+
+        <div class="col-sm">
+                <img src="assets/img/product2.png" class="py-5" alt="">
+        </div>
+
+        <div class="col-sm">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci, incidunt necessitatibus ut libero corporis provident consequuntur iusto ipsa at, perspiciatis, impedit accusantium dolor. Corporis deleniti distinctio architecto! Veniam, fugit ratione.</p>
+        </div>
+</div>
+
+
+
+
+
+
+<!-- Require Footer -->
 <?php require_once("inc/footer.php") ?>    
 </body>
 </html>
