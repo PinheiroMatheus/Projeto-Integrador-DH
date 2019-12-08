83cb6ac95507f8ab922eb6588255b350bccd97db
unknown <matheus.sousa247@hotmail.com>
2019-12-08 19:11:08 -0300

    Making Contato.php responsive

--- contato.php
+++ contato.php
@@ -5,47 +5,49 @@
     <?php require_once("inc/head.php") ?>
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <title>Document</title>
+    <link rel="stylesheet" href="css/stylecopy.css">
 </head>
 <body>
     <?php require_once("inc/header.php") ?>
-    <div class="container text-center mt-5" id="titulo">
-        <h1><b>Contate-nos</b></h1>
-    </div>  
-    <div class="container row px-5 py-5">
-        <div class="col-sm-6 col-md-6 col-lg-6 col-xs-12">
-            <h3>Telefone</h3>
-            <span>(11) 9xxxx - xxxx</span>
-            <hr>
-            <h3>Email</h3>
-            <span>suporte@beymind</span>
-            <hr>
-            <ul class="row pl-3" id="ul-contato">
-                <li><a href="http://instagram.com"><img src="assets/img/instagram-logo.png" width="40px" class="pr-2" alt="" class=""></a></li>
-                <li><a href="http://facebook.com"><img src="assets/img/facebook.logo.png" width="40px" class="pr-2" alt="" class=""></a></li>
-                <li><a href="http://twitter.com"><img src="assets/img/twitter-logo.png" width="40px" class="pr-2" alt="" class=""></a></li>
-            </ul>
+    
+    <div class="conatiner px-5" id="cont-master">
+        <div class="row text-center mt-5" id="div-titulo">
+            <h1><b>Contate-nos</b></h1>
         </div>
-        <div class="col-sm-6 col-md-6 col-lg-6 col-xs-12">
-            <div class="row pl-5">
-            <h3>Escreva para nós</h3>
+        <div class="row">
+            <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 mt-5">
+                <h3>Telefone</h3>
+                <span>(11) 9xxxx - xxxx</span>
+                <hr>
+                <h3>Email</h3>
+                <span>suporte@beymind</span>
+                <hr>
+                <div>
+                    <a href="http://instagram.com"><img src="assets/img/instagram-logo.png" width="40px" class="pr-2" alt="" class=""></a>
+                    <a href="http://facebook.com"><img src="assets/img/facebook.logo.png" width="40px" class="pr-2" alt="" class=""></a>
+                    <a href="http://twitter.com"><img src="assets/img/twitter-logo.png" width="40px" class="pr-2" alt="" class=""></a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 mt-5">
             <form>
-                <div class="form-row">
-                    <div class="col-6">
+                <h3>Escreva para nós</h3>
+                <div class="form-row mt-3">
+                    <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 mb-4">
                         <input type="text" class="form-control" placeholder="Nome">
                     </div>
-                    <div class="col-6">
-                        <input type="text" class="form-control" placeholder="Sobrenome">
+                    <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 mb-4">
+                        <input type="email" class="form-control" placeholder="Email">
                     </div>
-                    <div class="col-12 pt-4 form-group" id="form-box">
+                    <div class="col-12 form-group" id="form-box">
                         <textarea type="text" class="form-control" placeholder="Conteúdo" rows="6"></textarea>
                     </div>
                 </div>
             </form>
             </div>
         </div>
-    </div>
-    <div class="container-fluid my-5 px-5" id="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14629.55518769143!2d-46.6889560302246!3d-23.55447584999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m3!3e0!4m0!4m0!5e0!3m2!1spt-BR!2sbr!4v1575298739872!5m2!1spt-BR!2sbr" width="100%" height="350px" frameborder="0" style="border:1;" allowfullscreen=""></iframe>
+        <div class="row my-5">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14629.55518769143!2d-46.6889560302246!3d-23.55447584999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m3!3e0!4m0!4m0!5e0!3m2!1spt-BR!2sbr!4v1575298739872!5m2!1spt-BR!2sbr" width="100%" height="350px" frameborder="0" style="border:1;" allowfullscreen=""></iframe>
+        </div>
     </div>
 
     <?php require_once("inc/footer.php") ?>
